9e5f0733d159da08b536282b700cff973894f2e5
Arsum Elahi <arsumelahi@Arsums-MacBook-Pro.local>
2025-07-30 22:04:21 +0500

    removed unused imports

--- src/app/providers/RootProvider.tsx
+++ src/app/providers/RootProvider.tsx
@@ -1,17 +1,15 @@
 
 import { DefaultTheme } from "@/shared/config/theme";
 import { ApolloClientProvider } from "./apollo";
-import { ConfigProvider, Switch, theme } from "antd";
-import React, { useState } from "react";
+import { ConfigProvider } from "antd";
+import React from "react";
 
 export const RootProvider: React.FC<{children: React.ReactNode}> = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false);
-
+ 
   return (
     <ConfigProvider
       theme={{
-        ...DefaultTheme,
-       algorithm: darkMode ? theme.darkAlgorithm : theme.defaultAlgorithm
+        ...DefaultTheme
       }}
     >
     <ApolloClientProvider>
